c707434e0fa6bc9a550aee02e7f1d6079c63bddf
Alexandre3000z <joaoalexandrems3000@gmail.com>
2024-09-30 17:06:39 -0300

    a

--- src/app/cabecalho/cabecalho.component.ts
+++ src/app/cabecalho/cabecalho.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cabecalho',
   templateUrl: './cabecalho.component.html',
-  styleUrls: ['./cabecalho.component.css']
+  styleUrls: ['./cabecalho.component.css'],
 })
-export class CabecalhoComponent implements OnInit {
-
+export class CabecalhoComponent implements OnInit, OnDestroy {
   teste = 'TESTANDO123';
-  constructor() { }
+  horaAtual: string = '';
+  private subscription: Subscription | undefined;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.atualizarRelogio();
+    this.subscription = interval(1000).subscribe(() => this.atualizarRelogio());
   }
 
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  private atualizarRelogio(): void {
+    const agora = new Date();
+    const horas = agora.getHours().toString().padStart(2, '0');
+    const minutos = agora.getMinutes().toString().padStart(2, '0');
+    const segundos = agora.getSeconds().toString().padStart(2, '0');
+    this.horaAtual = `${horas}:${minutos}:${segundos}`;
+  }
 }
